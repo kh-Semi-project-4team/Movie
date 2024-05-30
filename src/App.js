0aa54c3ced3b48ce41724381408f0db728b09fb9
aappoo0109 <aappoo0109@gmail.com>
2024-05-30 18:25:47 +0900

    poster

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import SliderComponent from './components/mainpage/header/SliderComponent';
+import MainPage from './components/mainpage/contents/bestmovie';
 
 function App() {
   return (
     <div className="App">
       <SliderComponent />
+      <hr/>
+      <MainPage/>
     </div>
   );
 }

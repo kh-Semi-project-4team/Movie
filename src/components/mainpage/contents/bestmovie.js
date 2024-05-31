80a14976da7e50d7f87709918f4e62795047488e
aappoo0109 <aappoo0109@gmail.com>
2024-05-31 18:16:23 +0900

    인기작 5개

--- src/components/mainpage/contents/bestmovie.js
+++ src/components/mainpage/contents/bestmovie.js
@@ -31,16 +31,6 @@ function BestMovie() {
       <header className={styles.title1}>Best Movie!</header>
       <div className={styles.image}>
         <div className={styles.imageTd}>
-          <h3>범죄도시</h3>
-          {poster && (
-            <a href={`https://www.themoviedb.org/movie/1017163-4`}>
-              <div className={styles.bestImgContainer}>
-                <img className={styles.img} src={`https://media.themoviedb.org/t/p/original/h1YarEjeYurkAwXgfY1RDMVCiin.jpg`} alt="범죄도시"/>
-              </div>
-            </a>
-          )}
-        </div>
-        <div className={styles.imageTd}>
           <h3>혹성탈출</h3>
           {poster && (
             <a href={`https://www.themoviedb.org/movie/653346-kingdom-of-the-planet-of-the-apes`}>
@@ -51,19 +41,29 @@ function BestMovie() {
           )}
         </div>
         <div className={styles.imageTd}>
-          <h3>설계자</h3>
+          <h3>시빌 워</h3>
           {poster && (
-            <a href={`https://www.themoviedb.org/movie/865910`}>
+            <a href={`https://www.themoviedb.org/movie/929590-civil-war`}>
               <div className={styles.bestImgContainer}>
-                <img className={styles.img} src={`https://media.themoviedb.org/t/p/original/qcnrlYPXMh7gLAGzloMS7tNfHi2.jpg`} alt="설계자"/>
+                <img className={styles.img} src={`https://media.themoviedb.org/t/p/w220_and_h330_face/sh7Rg8Er3tFcN9BpKIPOMvALgZd.jpg`} alt="범죄도시"/>
               </div>
             </a>
           )}
         </div>
         <div className={styles.imageTd}>
-          <h3>이프,상상의 친구</h3>
+          <h3>고질라 X 콩: 뉴 엠파이어</h3>
           {poster && (
-            <a href={`https://www.themoviedb.org/movie/639720-if`}>
+            <a href={`https://www.themoviedb.org/movie/823464-godzilla-x-kong-the-new-empire?language=ko`}>
+              <div className={styles.bestImgContainer}>
+                <img className={styles.img} src={`https://media.themoviedb.org/t/p/w220_and_h330_face/4z1VMmlxHrziG45901esjB4dpIa.jpg`} alt="설계자"/>
+              </div>
+            </a>
+          )}
+        </div>
+        <div className={styles.imageTd}>
+          <h3>스턴트맨</h3>
+          {poster && (
+            <a href={`https://www.themoviedb.org/movie/746036-the-fall-guy?language=ko`}>
               <div className={styles.bestImgContainer}>
                 <img className={styles.img} src={`https://media.themoviedb.org/t/p/original/9GAOhSzXjXJR4AxYCa2AMzMGPVg.jpg`} alt="이프, 상상의 친구"/>
               </div>
@@ -71,11 +71,11 @@ function BestMovie() {
           )}
         </div>
         <div className={styles.imageTd}>
-          <h3>챌린저스</h3>
+          <h3>퓨리오사: 매드맥스 사가</h3>
           {poster && (
-            <a href={`https://www.themoviedb.org/movie/937287-challengers`}>
+            <a href={`https://www.themoviedb.org/movie/786892-furiosa-a-mad-max-saga?language=ko`}>
               <div className={styles.bestImgContainer}>
-                <img className={styles.img} src={`https://media.themoviedb.org/t/p/original/iUDG0gsPZxNm9IOLqpIUZiUMVMZ.jpg`} alt="챌린저스"/>
+                <img className={styles.img} src={`https://media.themoviedb.org/t/p/w220_and_h330_face/zaUFDdJidS4Nyyd6jb2Ok3Kq4Vo.jpg`} alt="챌린저스"/>
               </div>
             </a>
           )}

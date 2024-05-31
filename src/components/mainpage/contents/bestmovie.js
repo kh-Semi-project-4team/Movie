e0238880f9fd5ee043fe04e05c9b2f57b4fc7cc8
aappoo0109 <aappoo0109@gmail.com>
2024-05-31 16:35:51 +0900

    poster2

--- src/components/mainpage/contents/bestmovie.js
+++ src/components/mainpage/contents/bestmovie.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import styles from './bestMovieComponent.module.css';
-
+import styles from './bestmovie.module.css';
 
 function MainPage() {
   const [poster, setPoster] = useState(null);
@@ -28,33 +27,60 @@ function MainPage() {
   }, []);
 
   return (
-    <div>
-      <header>Best Movie!</header>
-      <tr>
-        <td>
+    <div className={styles.mainposter}>
+      <header className={styles.title1}>Best Movie!</header>
+  <tr className={styles.image}>
+    <td>
+      <h3>범죄도시</h3>
       {poster && (
         <a href={`https://www.themoviedb.org/movie/1017163-4`}>
-          <div className="best_img_1">
-          <img src={`https://media.themoviedb.org/t/p/original/h1YarEjeYurkAwXgfY1RDMVCiin.jpg`}/>
+          <div className={styles.best_img_container}>
+            <img src={`https://media.themoviedb.org/t/p/original/h1YarEjeYurkAwXgfY1RDMVCiin.jpg`}/>
           </div>
         </a>
       )}
-       </td>
-       <td>
+    </td>
+    <td>
+    <h3>혹성탈출</h3>
       {poster && (
         <a href={`https://www.themoviedb.org/movie/653346-kingdom-of-the-planet-of-the-apes`}>
-          <img src={`https://media.themoviedb.org/t/p/w220_and_h330_face/plNOSbqkSuGEK2i15A5btAXtB7t.jpg`} />
+          <div className={styles.best_img_container}>
+            <img src={`https://media.themoviedb.org/t/p/original/plNOSbqkSuGEK2i15A5btAXtB7t.jpg`}/>
+          </div>
         </a>
       )}
-       </td>
-       <td>
+    </td>
+    <td>
+    <h3>설계자</h3>
       {poster && (
-        <a href={`https://www.themoviedb.org/movie/746036-the-fall-guy`}>
-          <img src={`https://media.themoviedb.org/t/p/w220_and_h330_face/aUZ176q7ldMaPLYVnmaFWK4CApE.jpg`} />
+        <a href={`https://www.themoviedb.org/movie/865910`}>
+          <div >
+            <img src={`https://media.themoviedb.org/t/p/original/qcnrlYPXMh7gLAGzloMS7tNfHi2.jpg`} />
+          </div>
         </a>
       )}
-       </td>
-      </tr>
+    </td>
+    <td>
+    <h3>이프,상상의 친구</h3>
+      {poster && (
+        <a href={`https://www.themoviedb.org/movie/639720-if`}>
+          <div >
+            <img src={`https://media.themoviedb.org/t/p/original/9GAOhSzXjXJR4AxYCa2AMzMGPVg.jpg`} />
+          </div>
+        </a>
+      )}
+    </td>
+    <td>
+    <h3>챌린저스</h3>
+      {poster && (
+        <a href={`https://www.themoviedb.org/movie/937287-challengers`}>
+          <div >
+            <img src={`https://media.themoviedb.org/t/p/original/iUDG0gsPZxNm9IOLqpIUZiUMVMZ.jpg`} />
+          </div>
+        </a>
+      )}
+    </td>
+  </tr>
     </div>
   );
 }

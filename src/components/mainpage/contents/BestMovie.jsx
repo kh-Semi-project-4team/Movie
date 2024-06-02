34fd58d27e3f4cb79d28d5231a82be96679b059d
teeput <ciyi1594@gmail.com>
2024-06-02 21:11:43 +0900

    Best Movie 완성

--- src/components/mainpage/contents/BestMovie.jsx
+++ src/components/mainpage/contents/BestMovie.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './css/BestMovie.module.css';
 import useTmdbDataPull from '../useTmdbDataPull';
 
@@ -7,11 +7,14 @@ function BestMovie() {
 
   const renderStars = (voteAverage) => {
     const fullStars = Math.floor(voteAverage / 2);
-    const halfStar = voteAverage % 2 >= 1 ? 1 : 0;
+    const hasHalfStar = (voteAverage / 2) % 1 >= 0.5;
 
     const stars = [];
     for (let i = 0; i < fullStars; i++) {
       stars.push(<div key={i} className={styles.star}></div>);
+    }
+    if (hasHalfStar) {
+      stars.push(<div key="half" className={styles.halfStar}></div>);
     }
     return stars;
   };
@@ -28,7 +31,7 @@ function BestMovie() {
                 <img className={styles.img} src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} />
                 <div className={styles.else_container}>
                   <div className={styles.rating}>{renderStars(movie.vote_average)}</div>
-                  <div className={styles.vote_count}>{(movie.vote_count)} Vote!</div>
+                  <div className={styles.vote_count}>{movie.vote_count} Vote!</div>
                 </div>
               </div>
             </a>

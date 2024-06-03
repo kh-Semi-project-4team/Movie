9fb0ea25d03ab950b47dfd0bad17eecfdbc34a68
TEEPUT <ciyi1594@gmail.com>
2024-06-03 13:26:51 +0900

    네비게이션 최종 수정 완성

--- src/components/login/navbar/NavBar.jsx
+++ src/components/login/navbar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
+import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
 import styles from './css/Navbar.module.css';
 import btnImg from './img/btnImg.png';
 import closeImg from './img/closeImg.png';
@@ -8,11 +9,12 @@ const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [isZoomed, setIsZoomed] = useState(false);
   const [navBackground, setNavBackground] = useState(false);
+  const [activeSection, setActiveSection] = useState('');
   const navigate = useNavigate();
 
-  const handleMenuToggle = () => {
-    setMenuOpen(!menuOpen);
-    setIsZoomed(!isZoomed);
+  const openNav = () => {
+    document.getElementById("myNav").classList.toggle(styles.menu_width);
+    document.querySelector(`.${styles.custom_menu_btn}`).classList.toggle(styles.menu_btn_style);
   };
 
   const handleMenuClose = () => {
@@ -25,6 +27,18 @@ const NavBar = () => {
   };
 
   const handleScroll = () => {
+    const sections = document.querySelectorAll('section');
+    const scrollPosition = window.scrollY + 200; // Adjust based on your navbar height
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+        setActiveSection(section.getAttribute('id'));
+      }
+    });
+
     const bestMovieSection = document.getElementById('best-movie');
     if (bestMovieSection) {
       const bestMovieOffset = bestMovieSection.offsetTop;
@@ -47,27 +61,68 @@ const NavBar = () => {
   return (
     <nav className={`${styles.navbar} ${navBackground ? styles.navbarScrolled : ''}`}>
       <ul className={styles.navbarLinks}>
-        <li className={styles.navbarLogo} onClick={handleLogoClick}>
-          <img src='/image/logo.png' className={styles.logo} alt='Logo'/>
+        <li onClick={handleLogoClick}> 
+          <img src='/image/logo.png' className={styles.logo} alt='Logo' onClick={handleLogoClick}/>
         </li>
-        <li><NavLink exact to="/" className={({ isActive }) => (isActive ? 'active' : '')}>Home</NavLink></li>
-        <li><NavLink to="/mainpage" className={({ isActive }) => (isActive ? 'active' : '')}>MainPage</NavLink></li>
-        <li><NavLink to="/subpage" className={({ isActive }) => (isActive ? 'active' : '')}>SubPage</NavLink></li>
-        <li><NavLink to="/login" className={({ isActive }) => (isActive ? 'active' : '')}>Login</NavLink></li>
-        <button className={styles.MenuButton} onClick={handleMenuToggle}>
-          <img src={btnImg} className={styles.btnImg} alt='Menu'/>
-        </button>
+        <li className={styles.nav_gap_none}>
+          <ScrollLink
+            to="home-section"
+            smooth={true}
+            duration={500}
+            className={({ isActive }) => (activeSection === 'home-section' ? styles.active : '')}
+          >
+            Home
+          </ScrollLink>
+        </li>
+        <li className={styles.nav_gap_none}>
+          <ScrollLink
+            to="Best-section"
+            smooth={true}
+            duration={500}
+            className={({ isActive }) => (activeSection === 'Best-section' ? styles.active : '')}
+          >
+            Best
+          </ScrollLink>
+        </li>
+        <li className={styles.nav_gap_none}>
+          <ScrollLink
+            to="ComingSoon-section"
+            smooth={true}
+            duration={500}
+            className={({ isActive }) => (activeSection === 'ComingSoon-section' ? styles.active : '')}
+          >
+            ComingSoon
+          </ScrollLink>
+        </li>
+        <li className={styles.nav_gap_none}>
+          <ScrollLink
+            to="Kategorie-section"
+            smooth={true}
+            duration={500}
+            className={({ isActive }) => (activeSection === 'Kategorie-section' ? styles.active : '')}
+          >
+            Kategorie
+          </ScrollLink>
+        </li>
+        <li className={styles.nav_gap_none}>
+          <div className={styles.custom_menu_btn}>
+            <button onClick={openNav}>
+              <span className={styles.s_1}> </span>
+              <span className={styles.s_2}> </span>
+              <span className={styles.s_3}> </span>
+            </button>
+            <div id="myNav" className={styles.overlay}>
+              <div className={styles.overlay_content}>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/login">Login</NavLink>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className={styles.mobile}>
+          ---님{/* session에 로그인정보가 있으면 표시 없으면 로그인 창으로 이동*/}
+        </li>
       </ul>
-      <div className={`${styles.menuPage} ${menuOpen ? styles.menuPageOpen : ''} ${isZoomed ? styles.zoomed : ''}`}>
-        <button className={styles.CloseButton} onClick={handleMenuClose}>
-          <img src={closeImg} className={styles.closeImg} alt='Close'/>
-        </button>
-        <ul>
-          <li><NavLink to="/mainpage">MainPage</NavLink></li><br/><br/>
-          <li><NavLink to="/subpage">SubPage</NavLink></li><br/><br/>
-          <li><NavLink to="/login">Login</NavLink></li>
-        </ul>
-      </div>
     </nav>
   );
 };

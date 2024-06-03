682a9b5d24fe4911f7af3b6f0856ab34f052b1d1
juanalee <optihyuny07@gmail.com>
2024-06-03 18:32:02 +0900

    출연진 정보 추가

--- src/components/subpage/SubPage.jsx
+++ src/components/subpage/SubPage.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import HeaderImage from './header/HeaderImage';
 import MovieDetails from './contents/MovieDetails';
 import useMovieDataPullById from './useMovieDataPullById';
+import CastDetails from './contents/CastDetails';
 
 export default function SubPage() {
-    const movieId = sessionStorage.getItem('movieId') || 653346;
-    const { movieData, videos } = useMovieDataPullById(movieId);
-  
-    return (
-      <div>
-        <HeaderImage movieId={movieId} />
-        {movieData && <MovieDetails movieData={movieData} videos={videos} />}
-      </div>
-    );
+  const movieId = sessionStorage.getItem('movieId') || 653346;
+  const { movieData, videos } = useMovieDataPullById(movieId);
+
+  return (
+    <div>
+      <HeaderImage movieId={movieId} />
+      {movieData && <MovieDetails movieData={movieData} videos={videos} />}
+      {movieData && <CastDetails movieId={movieId} />}
+    </div>
+  );
 }
---
+++ src/components/subpage/contents/CastDetails.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import useMovieCastById from '../useMovieCastById';
+import styles from './css/CastDetails.module.css';
+
+const CastDetails = ({ movieId }) => {
+  const { castDetails } = useMovieCastById(movieId);
+  console.log('Cast Details:', castDetails);
+
+  return (
+    <div className={styles.castDetailsContainer}>
+      <h2>출연진</h2>
+      <div className={styles.castList}>
+        {castDetails.map(member => (
+          <div key={member.id} className={styles.castMember}>
+            {member.profile_path && (
+              <img src={`https://image.tmdb.org/t/p/w185${member.profile_path}`} alt={member.name} className={styles.castPhoto} />
+            )}
+            <div className={styles.castInfo}>
+              <p className={styles.castName}>{member.name}</p>
+              <p className={styles.castRole}>{member.character}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CastDetails;

d0516a45bf5030b399d7e91f6e5d791cc2ca6b4e
TEEPUT <ciyi1594@gmail.com>
2024-06-05 19:41:03 +0900

    서브페이지 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from 'react-redux';
-import {store} from './components/login/login_main/store/store'; // Redux 스토어 불러오기
+import {store} from './components/login/login_main/store/store';
 import { Routes, Route } from 'react-router-dom';
 import SubPage from "./components/subpage/SubPage";
 import MainPage from "./components/mainpage/MainPage";
@@ -14,7 +14,7 @@ function App() {
     <Provider store={store}>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/subpage" element={<SubPage />} />
+        <Route path="/subpage/:movieId" element={<SubPage />} />
         <Route path="/login" element={<LoginMain />} />
         <Route path="/login/LoginApp" element={<LoginApp />} />
         <Route path="/login/Register" element={<Register />} /> 

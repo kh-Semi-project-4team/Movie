8482efcede910841329977fa2733fd6c9a4f567f
teeput <ciyi1594@gmail.com>
2024-06-07 20:44:15 +0900

    빌드 테스트

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-    moduleFileExtensions: ['js', 'jsx'],
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-    transformIgnorePatterns: [
-      '/node_modules/(?!axios).+\\.js$'
-    ],
-  };
-  
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  testEnvironment: 'jsdom'
+};
